7b0b221fb3ff028707d21556dd850cd74d652af3
Mert KARTAL <179147337+Mert-Kartal@users.noreply.github.com>
2025-07-31 00:36:56 +0300

    Add helmet middleware for enhanced security in main.ts and update package.json and package-lock.json to include helmet dependency.

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
